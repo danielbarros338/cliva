d5c3776ae75c27fd3f02e4fde3f9b42926f3eefc
Daniel Barros <danielbarros338@gmail.com>
2021-12-30 00:12:32 -0300

    Criado componente para o player do Youtube

---
+++ src/components/YoutubePlayer.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <iframe 
+          width="560" 
+          height="315" 
+          src="https://www.youtube.com/embed/yaD8uLygi1g" 
+          title="YouTube video player" 
+          frameborder="0" 
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+          allowfullscreen
+        ></iframe>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "YoutubePlayer"
+}
+</script>
+
+<style scoped>
+
+</style>
