a4b39a9b3affada18875524abf39baf7ecad5dee
Daniel Barros <danielbarros338@gmail.com>
2021-12-30 23:43:15 -0300

    Modificada cor svg facebook

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,37 @@
   <div class="home">
     <div class="container1">
       <img class="logoCliva" :src="require('@./../../public/images/cliva.svg')" alt="Logotipo Cliva">
-      <Diary />
+      <span>
+        <a>
+          <img class="portal_icons" :src="require(`@./../../public/images/instagram.svg`)" alt="Facebook">
+        </a>
+      </span>
     </div>
   </div>
 </template>
 
 <script>
-import Diary from "@/components/Diary.vue";
+// import Diary from "@/components/Diary.vue";
 
 export default {
   name: "Home",
+  data() {
+    return {
+      url: [
+        "https://www.facebook.com/bandacliva/",
+        "https://www.instagram.com/clivaoficial/",
+        "https://www.youtube.com/channel/UC97Uz26S362xo_hlusOQhMQ"
+      ],
+      svgs: [
+        "facebook.svg",
+        "instragram.svg",
+        "youtube.svg"
+        // "whatsapp.svg"
+      ]
+    }
+  },
   components: {
-    Diary
+    // Diary
   }
 }
 </script>
@@ -24,6 +43,12 @@ export default {
 }
 .container1 img {
   width: 100vw;
+}
+
+@media (max-width: 450px) {
+  .portal_icons {
+    height: 30px;
+  }
 }
 
 @media (min-width: 1000px) {
