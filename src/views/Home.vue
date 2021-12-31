8a3d7822472502d5b90511bd0f0d00d40d8e0988
Daniel Barros <danielbarros338@gmail.com>
2021-12-30 23:53:39 -0300

    implementado loop para exibicao dos icones dos portais

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="home">
     <div class="container1">
-      <img class="logoCliva" :src="require('@./../../public/images/cliva.svg')" alt="Logotipo Cliva">
-      <span>
-        <a>
-          <img class="portal_icons" :src="require(`@./../../public/images/youtube.svg`)" alt="Facebook">
+      <img class="logo_cliva" :src="require('@./../../public/images/cliva.svg')" alt="Logotipo Cliva">
+      <div class="portal_containers">
+        <a v-for="(portal, index) of portals" :key="index" :href="portal.url">
+          <img class="portal_icons" :src="require(`@./../../public/images/${portal.svg}`)" alt="Facebook">
         </a>
-      </span>
+      </div>
     </div>
   </div>
 </template>
@@ -18,16 +18,17 @@ export default {
   name: "Home",
   data() {
     return {
-      url: [
-        "https://www.facebook.com/bandacliva/",
-        "https://www.instagram.com/clivaoficial/",
-        "https://www.youtube.com/channel/UC97Uz26S362xo_hlusOQhMQ"
-      ],
-      svgs: [
-        "facebook.svg",
-        "instragram.svg",
-        "youtube.svg"
-        // "whatsapp.svg"
+      portals: [
+        { 
+          url: "https://www.facebook.com/bandacliva/",
+          svg: "facebook.svg"
+        },{
+          url: "https://www.instagram.com/clivaoficial/",
+          svg: "instagram.svg"
+        },{
+          url: "https://www.youtube.com/channel/UC97Uz26S362xo_hlusOQhMQ",
+          svg: "youtube.svg"
+        } 
       ]
     }
   },
@@ -41,8 +42,12 @@ export default {
 .container1 {
   background-color: var(--color-primary);
 }
-.container1 img {
+.container1 .logo_cliva {
   width: 100vw;
+}
+.portal_containers {
+  display: flex;
+  justify-content: space-evenly;
 }
 
 @media (max-width: 450px) {
