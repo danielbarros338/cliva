9d13ea68f31d2e9706cde2f3e7cd5303289d8915
Daniel Barros <danielbarros338@gmail.com>
2022-01-05 15:10:59 -0300

    Implementado componente de shows

--- src/components/Diary.vue
+++ src/components/Diary.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="container">
-
+    <h1>Próximos shows</h1>
+    <div class="container_shows">
+      <table>
+        <tr>
+          <th>Lethal Death Fest</th>
+          <td>Rock Experience - Lapa, RJ</td>
+        </tr>
+        <tr>
+          <th>Festival Transborde</th>
+          <td>Santa Cruz, RJ</td>
+        </tr>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -8,4 +20,20 @@
 export default {};
 </script>
 
-<style scoped></style>
+<style scoped>
+@media (max-width: 450px) {
+  .container_shows {
+    margin-top: 20px;
+  }
+  .container_shows,
+  .container_shows table {
+    width: 100vw;
+  }
+  .container_shows th{
+    text-align: start;
+  }
+  .container_shows td {
+    text-align: center;
+  }
+}
+</style>
